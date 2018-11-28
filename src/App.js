dc645091daab0112be302a1792a8719f4991ac57
VadimKh <khoroshiltsev.vadim@gmail.com>
2018-11-28 16:40:02 +0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="App-nav">
+          <Link to="/" className="App-link">Home</Link>
+          <Link to="/about" className="App-link">About</Link>
+        </nav>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </header>
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <p>Home page</p>
+)
+
+const About = () => (
+  <p>About page</p>
+)
 
 export default App;
